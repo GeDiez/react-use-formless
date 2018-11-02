367326c79bf4cddbc59ff7b60b859723410d2ca5
Gibran Lopez Morales <gibran@michelada.io>
2018-11-01 18:36:12 -0600

    add landing-page

---
+++ landing-page/precache-manifest.e09732bc0c50a5c83df2fd3d6a9dc983.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ceb2eb80c7ced322fee",
+    "url": "/static/js/main.0ceb2eb8.chunk.js"
+  },
+  {
+    "revision": "919ae5e53a25af6ef8ec",
+    "url": "/static/js/1.919ae5e5.chunk.js"
+  },
+  {
+    "revision": "919ae5e53a25af6ef8ec",
+    "url": "/static/css/1.cc7ef91e.chunk.css"
+  },
+  {
+    "revision": "dd1d34809e11695226aeda19b8a9d937",
+    "url": "/index.html"
+  }
+];
