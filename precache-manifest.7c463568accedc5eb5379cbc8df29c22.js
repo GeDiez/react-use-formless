3c9c0c66c58bc6daf84ecaf1fcab0da92ed3a466
Gibran Lopez Morales <gibran@michelada.io>
2019-07-19 20:49:52 -0500

    Updates

---
+++ precache-manifest.7c463568accedc5eb5379cbc8df29c22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9bae3f219c4902767d4",
+    "url": "/react-use-formless/static/js/runtime~main.d9bae3f2.js"
+  },
+  {
+    "revision": "a9f06b590938e2dfd1c0",
+    "url": "/react-use-formless/static/js/main.a9f06b59.chunk.js"
+  },
+  {
+    "revision": "7fea0a64e17e63c068f6",
+    "url": "/react-use-formless/static/js/1.7fea0a64.chunk.js"
+  },
+  {
+    "revision": "a9f06b590938e2dfd1c0",
+    "url": "/react-use-formless/static/css/main.e4976e35.chunk.css"
+  },
+  {
+    "revision": "7fb4fb8d4d1835a9dbf50bcd2e4f150e",
+    "url": "/react-use-formless/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-use-formless/precache-manifest.1f6869d4c156d2100a7f64fbe985d231.js"
+  "/react-use-formless/precache-manifest.7c463568accedc5eb5379cbc8df29c22.js"
 );
 
 workbox.clientsClaim();
